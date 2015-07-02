1c207bf592648d14fb4f7a1abeb3eed3aebf37b9
Hugo Artemio Gonzalez <hagonzalez94@gmail.com>
2015-07-01 23:57:17 -0700

    added iTunes ScriptingBridge file and playlist reader code.

--- iTunes Playlist Converter/ViewController.m
+++ iTunes Playlist Converter/ViewController.m
@@ -7,13 +7,28 @@
 //
 
 #import "ViewController.h"
+#import "iTunes.h"
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
     // Do any additional setup after loading the view.
+    iTunesApplication *iTunes = [SBApplication applicationWithBundleIdentifier:@"com.apple.iTunes"];
+    if ([iTunes isRunning]) {
+        NSArray *sources = [[iTunes sources] get];
+        NSArray *libs = [sources filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"kind == %i", iTunesESrcLibrary]];
+        if ([libs count]) {
+            iTunesSource *src = [libs objectAtIndex:0];
+            NSArray *libraryLists = [[src libraryPlaylists] get];
+            for (iTunesLibraryPlaylist *list in libraryLists) {
+                NSArray *listTracks = [[list fileTracks] get];
+                for (iTunesTrack *listTrack in listTracks) {
+                    NSLog(@"%@", listTrack.name);
+                }
+            }
+        }
+    }
 }
 
 - (void)setRepresentedObject:(id)representedObject {
